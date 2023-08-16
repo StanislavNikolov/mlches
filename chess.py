59ab9828cd5899bf2912a42353ac191fe40bfa91
Stanislav Ch. Nikolov <stanislav.ltb@gmail.com>
2023-08-16 16:38:48 +0300

    Optimized nn input encoding

--- chess.py
+++ chess.py
@@ -4,37 +4,32 @@ import pickle
 from pathlib import Path
 
 def parse_board(board: str):
-    squares = np.zeros(64)
+    squares = np.zeros(64 * 6)
     char2sqaure = {
-        #  0 empty
-        'K':  1, #  1 white king
-        'Q':  2, #  2 white queen
-        'R':  3, #  3 white rook
-        'B':  4, #  4 white bishop
-        'N':  5, #  5 white knight
-        'P':  6, #  6 white pawn
-        'k':  7, #  7 black king
-        'q':  8, #  8 black queen
-        'r':  9, #  9 black rook
-        'b': 10, # 10 black bishop
-        'n': 11, # 11 black knight
-        'p': 12, # 12 black pawn
+        # 'e': np.array([ 0, 0, 0, 0, 0, 0]), #    empty
+        'K': np.array([+1, 0, 0, 0, 0, 0]), #  1 white king
+        'Q': np.array([ 0,+1, 0, 0, 0, 0]), #  2 white queen
+        'R': np.array([ 0, 0,+1, 0, 0, 0]), #  3 white rook
+        'B': np.array([ 0, 0, 0,+1, 0, 0]), #  4 white bishop
+        'N': np.array([ 0, 0, 0, 0,+1, 0]), #  5 white knight
+        'P': np.array([ 0, 0, 0, 0, 0,+1]), #  6 white pawn
+        'k': np.array([-1, 0, 0, 0, 0, 0]), #  7 black king
+        'q': np.array([ 0,-1, 0, 0, 0, 0]), #  8 black queen
+        'r': np.array([ 0, 0,-1, 0, 0, 0]), #  9 black rook
+        'b': np.array([ 0, 0, 0,-1, 0, 0]), # 10 black bishop
+        'n': np.array([ 0, 0, 0, 0,-1, 0]), # 11 black knight
+        'p': np.array([ 0, 0, 0, 0, 0,-1]), # 12 black pawn
     }
 
     idx = 0
     for c in board:
-        if c == '/': continue
-        elif c == '1': idx += 1
-        elif c == '2': idx += 2
-        elif c == '3': idx += 3
-        elif c == '4': idx += 4
-        elif c == '5': idx += 5
-        elif c == '6': idx += 6
-        elif c == '7': idx += 7
-        elif c == '8': idx += 8
-        else:
-            squares[idx] = char2sqaure[c]
-            idx += 1
+        if c == '/':
+            continue
+        if '1' <= c <= '8':
+            idx += ord(c) - ord('0')
+            continue
+        squares[idx*6:(idx+1)*6] = char2sqaure[c]
+        idx += 1
     
     return squares
 
@@ -53,6 +48,7 @@ def get_data(limit=None):
     count = len(data[:limit])
 
     already_parsed = Path("evals.bin").exists()
+    # already_parsed = False
     if not already_parsed:
         Path("evals.bin").touch()
         Path("squares.bin").touch()
@@ -60,7 +56,7 @@ def get_data(limit=None):
         print("Seems like chessData.csv has not been parsed alredy, parsing...")
 
     evals = np.memmap("evals.bin", dtype=np.float32, shape=(count, ))
-    squares = np.memmap("squares.bin", dtype=np.int8, shape=(count, 64))
+    squares = np.memmap("squares.bin", dtype=np.int8, shape=(count, 64*6))
     turns = np.memmap("turns.bin", dtype=np.int8, shape=(count,))
 
     if not already_parsed:
@@ -70,7 +66,7 @@ def get_data(limit=None):
             # Parse evaluation
             evaluation = evaluation.strip()
             if evaluation[0] == '#': evaluation = evaluation[1:] + '000'
-            evaluation = float(evaluation) / 100
+            evaluation = float(evaluation) / 1000
             if evaluation > 1.0: evaluation = 1
             if evaluation < -1.0: evaluation = -1
             evals[idx] = evaluation
--- train.py
+++ train.py
@@ -1,14 +1,14 @@
 import numpy as np
 import torch
 from torch import nn
-from chess import get_data
+from chess import get_data, parse_fen
 import time
 
 class MLP(nn.Module):
   def __init__(self):
     super().__init__()
     self.layers = nn.Sequential(
-      nn.Linear(64 * 13 + 1, 512),
+      nn.Linear(64 * 6 + 1, 512),
       nn.ReLU(),
       nn.Linear(512, 128),
       nn.ReLU(),
@@ -28,7 +28,7 @@ device = torch.device("cpu")
 torch.manual_seed(43)
 mlp = MLP().to(device)
 loss_function = nn.MSELoss()
-optimizer = torch.optim.Adam(mlp.parameters(), lr=0.1)
+optimizer = torch.optim.Adam(mlp.parameters(), lr=1e-4)
 
 param_count = 0
 for p in mlp.parameters():
@@ -39,19 +39,40 @@ load_begin = time.time()
 evals, squares, turns = get_data()
 print(f"get_data() took {time.time()-load_begin:.0f}s")
 
+def encode(squares, turns):
+    _s = torch.tensor(squares).float() # [B, 64*6]
+    # _s = torch.nn.functional.one_hot(_s, num_classes=13) # [B, 64, 13]
+    # _s = torch.flatten(_s, 1).type(torch.float32) # [B, 832]
+    _t = torch.tensor(turns).unsqueeze(1) # [B, 1]
+    return torch.cat((_s, _t), dim=1).to(device) # [B, 832+1]
+
+def val():
+    # https://lichess.org/Hj7N23o8/black#69 - +6.3
+    s1, t1 = parse_fen('1R6/p3k3/2p5/8/3b1rN1/5NK1/2Q3PP/q7 b - - 8 35')
+
+    # https://lichess.org/uZZ7jtZP/white#84 - -8.4
+    s2, t2 = parse_fen('4R3/kp6/p7/6pp/1r1q4/P6P/8/7K w - - 2 43')
+
+    # https://lichess.org/R9Zq7235/white#26 - +9999 mate in 1
+    s3, t3 = parse_fen('r1b1r1k1/pppp1ppp/1nn5/1B6/8/5N2/PP1N1PPP/4RRK1 w - - 2 14')
+
+
+    # print(s1)
+    xs = encode(np.array([s1, s2, s3]), np.array([t1, t2, t3]))
+    # print(xs[0])
+    with torch.no_grad():
+        preds = mlp(xs)
+        print(preds)
+
 
 for epoch in range(0, 10):
     print(f"====== starting epoch {epoch}")
     cnt = 0
     loss_sum = 0
-    batch_size = 500
+    batch_size = 1000
     last_print = time.time()
     for i in range(0, len(evals), batch_size):
-        _s = torch.tensor(np.array(squares[i:i+batch_size]), dtype=torch.int64) # [B, 64]
-        _s = torch.nn.functional.one_hot(_s, num_classes=13) # [B, 64, 13]
-        _s = torch.flatten(_s, 1).type(torch.float32) # [B, 832]
-        _t = torch.tensor(turns[i:i+batch_size]).unsqueeze(1) # [B, 1]
-        xs = torch.cat((_s, _t), dim=1).to(device) # [B, 832+1]
+        xs = encode(squares[i:i+batch_size], turns[i:i+batch_size])
         ys = torch.tensor(evals[i:i+batch_size]).unsqueeze(1).to(device)
 
         optimizer.zero_grad()
@@ -69,3 +90,5 @@ for epoch in range(0, 10):
             print(f'loss={loss_sum/(cnt):.5f}\ttpb={ns_per_board:.0f}ns\tboards={i+batch_size}')
             loss_sum = 0
             cnt = 0
+
+            # val()
